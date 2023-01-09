d6afee61d37098da3da9699e1d81d8571514ee6b
Sergei Bozhko <sbozhko@mpi-sws.org>
2023-01-09 18:33:26 +0100

    run on test dataset [Accuracy on test: 0.9566]
    Looks like a good result for a simple MLP with just one hidden layer

--- tests/mnist.rs
+++ tests/mnist.rs
@@ -7,6 +7,7 @@ mod tests {
         use rand::Rng;
         use mnist::*;
         use ndarray::prelude::*;
+        use std::io::{self, Write};
 
         use rustgrad::core::nonlinearity::NonLinearity::*;
         use rustgrad::mlp::layer::LayerSpec::*;
@@ -75,16 +76,18 @@ mod tests {
             println!("Constructing the model...");
             let mlp = MLP::new(
                 784, vec![
-                    FullyConnected(16), NonLinear(ReLu),
+                    FullyConnected(22), NonLinear(ReLu),
                     FullyConnected(10)
                 ]
             );
             let loss = Loss::with_multi_class_hinge_loss(&mlp);
 
             println!("Training the model...");
-            for i in 0..50000 {
-                println!("Loss = {:.8}, \t Iter = {}", loss.rand_batch_train(&mlp, &x_train, &y_train, 64, 0.25), i);
+            for i in 0..250_000 {
+                print!("\rLoss = {:.8}, \t Iter = {}", loss.rand_batch_train(&mlp, &x_train, &y_train, 64, 0.2), i);
+                io::stdout().flush().unwrap();
             }
+            println!();
 
             // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- //
             //                              Evaluate on dev data                               //
@@ -104,47 +107,47 @@ mod tests {
                     .collect::<Vec<f64>>()
                 ).collect::<Vec<Vec<f64>>>();
 
+            println!("--------------------------------Accuracy on dev data--------------------------------");
             let right_predictions = (0..dev).map( |i| {
                 let prediction : usize = mlp.eval(&x_dev[i]).iter().enumerate().max_by(|a, b| a.1.partial_cmp(b.1).unwrap()).unwrap().0;
                 let label : usize = y_dev[i].iter().enumerate().max_by(|a, b| a.1.partial_cmp(b.1).unwrap()).unwrap().0;
                 (prediction == label) as i32
             });
-            println!("--------------------------------Accuracy on dev data--------------------------------");
-            println!("Accuracy: {}", right_predictions.sum::<i32>() as f64 / dev as f64);
+            println!("Accuracy on dev: {}", right_predictions.sum::<i32>() as f64 / dev as f64);
 
             // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- //
             //                              Run on the test data                               //
             // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- //
 
-            // let test_data = Array3::from_shape_vec((10_000, 28, 28), tst_img)
-            //     .expect("Error converting images to Array3 struct")
-            //     .map(|x| *x as f32 / 256.);
+            let test_data = Array3::from_shape_vec((10_000, 28, 28), tst_img)
+                .expect("Error converting images to Array3 struct")
+                .map(|x| *x as f64 / 256.);
 
-            // let test_labels: Array2<f32> = Array2::from_shape_vec((10_000, 1), tst_lbl)
-            //     .expect("Error converting testing labels to Array2 struct")
-            //     .map(|x| *x as f32);
+            let test_labels: Array2<f64> = Array2::from_shape_vec((10_000, 1), tst_lbl)
+                .expect("Error converting testing labels to Array2 struct")
+                .map(|x| *x as f64);
 
-            // let x_test =
-            //     (0..test).map( |image_num|
-            //         test_data.slice(s![image_num, .., ..])
-            //         .iter().map( |x| *x as f64 )
-            //         .collect::<Vec<f64>>()
-            //     ).collect::<Vec<Vec<f64>>>();
+            let x_test =
+                (0..test).map( |image_num|
+                    test_data.slice(s![image_num, .., ..])
+                    .iter().map( |x| *x as f64 )
+                    .collect::<Vec<f64>>()
+                ).collect::<Vec<Vec<f64>>>();
 
-            // let y_test =
-            //     (0..test).map( |image_num|
-            //         test_labels.slice(s![image_num, ..])
-            //         .iter().map( |x| *x as f64 )
-            //         .collect::<Vec<f64>>()
-            //     ).collect::<Vec<Vec<f64>>>();
+            let y_test =
+                (0..test).map( |image_num|
+                    test_labels.slice(s![image_num, ..])
+                    .iter().flat_map( |x| one_hot(*x) )
+                    .collect::<Vec<f64>>()
+                ).collect::<Vec<Vec<f64>>>();
 
-            // println!("-------------------------------Accuracy on test data--------------------------------");
-            // for _ in 0..25 {
-            //     let i = rand::thread_rng().gen_range(0..test);
-            //     let prediction : usize = mlp.eval(&x_train[i]).iter().enumerate().max_by(|a, b| a.1.partial_cmp(b.1).unwrap()).unwrap().0;
-            //     let label : usize = y_train[i].iter().enumerate().max_by(|a, b| a.1.partial_cmp(b.1).unwrap()).unwrap().0;
-            //     println!("Model: {:?}, \tLabel: {:?}, \tMatch: {:?}", prediction, label, prediction == label);
-            // }
+            println!("-------------------------------Accuracy on test data--------------------------------");
+            let right_predictions = (0..test).map( |i| {
+                let prediction : usize = mlp.eval(&x_test[i]).iter().enumerate().max_by(|a, b| a.1.partial_cmp(b.1).unwrap()).unwrap().0;
+                let label : usize = y_test[i].iter().enumerate().max_by(|a, b| a.1.partial_cmp(b.1).unwrap()).unwrap().0;
+                (prediction == label) as i32
+            });
+            println!("Accuracy on test: {}", right_predictions.sum::<i32>() as f64 / test as f64);
 
         }
     }
